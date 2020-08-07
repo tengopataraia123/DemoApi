4e4aa7fdfd63cec346cc22fcd8d3fa31108b1c44
cybermachvi <cybermachvi@gmail.com>
2020-08-07 19:39:17 +0400

    ჩემი პირველი commit git-ის გამოყენებით

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask 
+from flask_restful import Api
+from flask_jwt import JWT
+
+from security import authenticate, identity
+
+from resources.item import ItemList, Item
+
+app = Flask(__name__)
+app.secret_key = "TheBestKeptSecret"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+jwt = JWT(app, authenticate, identity)
+api = Api(app)
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, "/items")
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+if __name__ == "__main__":
+    from db import db
+    db.init_app(app)
+
+    app.run(port = 5000, debug = True)
