3b77fd7d2af5540db6fcf32a2bae0ed0f70be09c
Zura <70130285+papiashviliz@users.noreply.github.com>
2020-09-07 20:45:37 +0400

    დავამატე ახალი მეთოდი სიხშირის განაწილების

--- README.md
+++ README.md
@@ -8,12 +8,26 @@
 
 მოთხოვნის მისამართი და მოთხოვნის body შეგიძლიათ ნახოთ მეთოდების ჩამონათვალში
 
-**Methods**:
-> ####endpoint: _/TemSenTok_
+##Methods:
+
+**TemSentenceTokenizer**
+>
 > სერვისი მოთხოვნილ წინადადებას უკეთებს ტოკენიზაციას და აბრუნებს ტოკენიზებულ სიას
 > ``` 
+> endpoint: _/TemSenTok_
 > type: GET 
 > body: {
 > "sentence" : "This is my demo sentence"
+> } 
+> ```
+> 
+**FrequencyDistribution**
+>
+> სერვისი მოთხოვნილ ტექსტს უკეთებს ტოკენიზაციას და აბრუნებს სიტყვების სიხშირის განაწილების სიას
+> ``` 
+> endpoint: _/FreqDist_
+> type: GET 
+> body: {
+> "text" : "This is my demo text"
 > } 
 > ```
--- resources/methods.py
+++ resources/methods.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource, reqparse
 from nltk.tokenize import word_tokenize
-
+from nltk.probability import FreqDist
 class TemSentenceTokenizer(Resource):
 
     parser = reqparse.RequestParser()
@@ -10,9 +10,10 @@ class TemSentenceTokenizer(Resource):
                         help="გთხოვთ შეიყვანოთ სწორი წინადადება")
 
     def get(self):
+        data = TemSentenceTokenizer.parser.parse_args()
+        sentence = data['sentence']
         try:
-            data = TemSentenceTokenizer.parser.parse_args()
-            sentence = data['sentence']
+
 
             tokenized_words = word_tokenize(sentence)
 
@@ -21,4 +22,22 @@ class TemSentenceTokenizer(Resource):
 
         else:
             return {'result': tokenized_words}, 200
+
+class FrequencyDistribution(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('text',
+                        type=str,
+                        required=True,
+                        help="გთხოვთ შეიყვანოთ სწორი ტექსტი")
+
+    def get (self):
+        data = FrequencyDistribution.parser.parse_args()
+        text = data['text']
+        clean_text=word_tokenize(text)
+        freqdist=FreqDist(clean_text)
+
+
+
+        return {'result': freqdist}
+
 
