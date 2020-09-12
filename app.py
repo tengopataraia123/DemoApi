4275c9d87e57d6ca4fa8d8e928c397f649f20fb6
temurchichua <cybermachvi@gmail.com>
2020-09-12 16:52:15 +0400

    added nltk tools

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 from flask import Flask, redirect
 from flask_restful import Api
 
+import nltk
+
+nltk.download('punkt')
+
 from resources.methods import TemSentenceTokenizer
 from resources.methods import FrequencyDistribution
 from resources.mariam_kvantaliani.methods import GroupingMultiplePatters
 from resources.chunk_CleanSentences import Chunk_CleanSentences
 
-
 app = Flask(__name__)
 api = Api(app)
+
 
 @app.route('/')
 def home():
     return redirect('https://github.com/temurchichua/DemoApi')
+
 
 api.add_resource(TemSentenceTokenizer, '/TemSenTok')
 api.add_resource(FrequencyDistribution, '/FreqDist')
@@ -20,4 +25,4 @@ api.add_resource(GroupingMultiplePatters, '/GMultiplePatterns')
 api.add_resource(Chunk_CleanSentences, '/Chunk')
 
 if __name__ == "__main__":
-    app.run(port = 5000, debug = True)
+    app.run(port=5000, debug=True)
