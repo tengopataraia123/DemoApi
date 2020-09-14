46e0c2c08a2080b547ced2074be20c4420a81b18
temurchichua <temur.chichua@iliauni.edu.ge>
2020-09-14 20:27:28 +0400

    მოვამზადე პროექტი დასაჰოსტად

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ api.add_resource(TemSentenceTokenizer, '/TemSenTok')
 api.add_resource(FrequencyDistribution, '/FreqDist')
 api.add_resource(GroupingMultiplePatters, '/GMultiplePatterns')
 api.add_resource(Chunk_CleanSentences, '/Chunk')
-api.add_resource(SentenceTokenize, '/sentence')
+api.add_resource(SentenceTokenizer, '/sentence')
 api.add_resource(FrequencyReturner, '/FreqReturner')
 
 if __name__ == "__main__":
